f59a563a965cabeca9eed42e2c7fa116510979c0
Lluc Koome Cebrià <lluckoomecebria@gmail.com>
2024-02-13 00:25:42 +0100

    TileChanges

--- Assets/ScriptsLluc/Seed.cs
+++ Assets/ScriptsLluc/Seed.cs
@@ -58,7 +58,7 @@ public static class Seed
         {false,true,true,false,false,false,true,false,false,false,true,true,true,false },
         {false,false,true,false,false,false,true,true,true,true,true,true,true,false },
         {false,false,true,false,false,false,true,false,false,false,true,true,true,false },
-        {false,true,true,true,true,true,true,true,true,false,false,false,true,false },
+        {false,true,true,true,true,true,true,true,true,false,false,true,false,false },
         {false,true,true,true,true,true,true,true,true,true,true,true,false,false },
         {false,false,false,false,false,false,false,false,false,false,false,false,false,false }
     };
